b613fbb3dfad2195ee57e8f7e15c082be25dd089
Collasperz <120536920+Collasperz@users.noreply.github.com>
2023-06-19 22:48:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import asyncio
 
 
 pygame.mixer.pre_init(44100, -16, 2, 512)
-mixer.init()
 pygame.init()
+mixer.init()
 pygame.font.init()
 my_font = pygame.font.Font('Pictures/Peepo.ttf', 50)
 text_surface = my_font.render("Slime of Time", False, (0,0,0))
